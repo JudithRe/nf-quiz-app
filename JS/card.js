9e07f3dd32946666ffacfd74164d9111cfa8c840
Judith Retzlaff <Judith.Retzlaff@gmx.net>
2023-06-20 16:35:03 +0200

    add multiple choice card

--- JS/card.js
+++ JS/card.js
@@ -1,5 +1,26 @@
 console.clear();
 
+// Variables for Bookmark Buttons
+
+const bookmarkButton1 = getElementbyDataJS("card__bookmark-button--1");
+const bookmarkButton2 = getElementbyDataJS("card__bookmark-button--2");
+const bookmarkButton3 = getElementbyDataJS("card__bookmark-button--3");
+const bookmarkButton4 = getElementbyDataJS("card__bookmark-button--4");
+const bookmarkButton5 = getElementbyDataJS("card__bookmark-button--5");
+
+// Variables for Answer Buttons
+
+const answerButton1 = getElementbyDataJS("card__answer-button--1");
+const answerButton2 = getElementbyDataJS("card__answer-button--2");
+const answerButton3 = getElementbyDataJS("card__answer-button--3");
+const answerButton4 = getElementbyDataJS("card__answer-button--4");
+const answerButton5 = getElementbyDataJS("card__answer-button--5");
+
+// Variables for Multiple Choice Buttons
+const multipleChoiceButton1 = getElementbyDataJS("card__option-button--1");
+const multipleChoiceButton2 = getElementbyDataJS("card__option-button--2");
+const multipleChoiceButton3 = getElementbyDataJS("card__option-button--3");
+
 // Functions
 
 function getElementbyDataJS(elementIdentifier) {
@@ -32,21 +53,19 @@ function toggleAnswerText(id) {
   }
 }
 
-// Variables for Bookmark Buttons
+function multipleChoiceAnswer(id) {
+  const button = document.querySelector(
+    `[data-js="card__option-button--${id}"]`
+  );
 
-const bookmarkButton1 = getElementbyDataJS("card__bookmark-button--1");
-const bookmarkButton2 = getElementbyDataJS("card__bookmark-button--2");
-const bookmarkButton3 = getElementbyDataJS("card__bookmark-button--3");
-const bookmarkButton4 = getElementbyDataJS("card__bookmark-button--4");
-const bookmarkButton5 = getElementbyDataJS("card__bookmark-button--5");
-
-// Variables for Answer Buttons
-
-const answerButton1 = getElementbyDataJS("card__answer-button--1");
-const answerButton2 = getElementbyDataJS("card__answer-button--2");
-const answerButton3 = getElementbyDataJS("card__answer-button--3");
-const answerButton4 = getElementbyDataJS("card__answer-button--4");
-const answerButton5 = getElementbyDataJS("card__answer-button--5");
+  if (button.classList.contains("--is-correct")) {
+    button.style.backgroundColor = "var(--highlight-color)";
+    button.style.animation = "vertical-shaking 0.35s 0.01s";
+  } else {
+    button.style.backgroundColor = "var(--wrong-highlight-color)";
+    button.style.animation = "horizontal-shaking 0.35s 0.01s";
+  }
+}
 
 // Event Listeners for Bookmark Buttons
 
@@ -82,4 +101,16 @@ answerButton4.addEventListener("click", () => {
 });
 answerButton5.addEventListener("click", () => {
   toggleAnswerText(5);
+});
+
+// Event Listeners for Multiple Choice Buttons
+
+multipleChoiceButton1.addEventListener("click", () => {
+  multipleChoiceAnswer(1);
+});
+multipleChoiceButton2.addEventListener("click", () => {
+  multipleChoiceAnswer(2);
+});
+multipleChoiceButton3.addEventListener("click", () => {
+  multipleChoiceAnswer(3);
 });
